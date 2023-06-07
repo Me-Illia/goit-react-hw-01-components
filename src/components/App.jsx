980daae598df55037402f14de9ce6ecf1af28243
Me-Illia <sq999@ukr.net>
2023-06-07 22:50:02 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import user from '../Json/user.json';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import data from '../Json/data.json';
+import FriendList from './FriendList/FriendList';
+import friends from '../Json/friends.json';
+import transactions from '../Json/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 import './App.css';
 
@@ -13,6 +19,10 @@ export const App = () => {
         location={user.location}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" data={data} />
+      <Statistics data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
