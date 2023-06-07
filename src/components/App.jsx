d99f9380f3de0fb468aa603a952dd4e27abb9614
Me-Illia <sq999@ukr.net>
2023-06-07 13:00:23 +0300

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import user from '../Json/user.json';
+import Profile from './Profile/Profile';
+
+import './App.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
     </div>
   );
 };
